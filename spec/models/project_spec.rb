1144ea9ef392e015b088893c17fb1efcc2d3d846
kaspernj <k@spernj.org>
2022-08-26 09:52:23 +0000

    Fixed focused spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe Project do
-  fit "doesn't add a presence validator on name because one is already defined directly on the model" do
+  it "doesn't add a presence validator on name because one is already defined directly on the model" do
     name_validators = Project.validators_on(:name).select { |validator| validator.kind == :presence }
 
     expect(name_validators).to have_attributes(length: 1)
